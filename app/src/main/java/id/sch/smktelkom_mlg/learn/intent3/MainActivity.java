95b5970a8b3757d33485fd7ecad94798ecae5bb1
septy <septydarmantya@gmail.com>
2016-10-09 13:31:12 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -26,5 +26,20 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
             }
         });
+
+        findViewById(R.id.imageViewSMS).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                composeSmsMessage("Pesan dari SMK Telkom Malang");
+            }
+
+            private void composeSmsMessage(String message) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra("sms_body", message);
+                if (intent.resolveActivity(getPackageManager()) != null)
+                    startActivity(intent);
+            }
+        });
     }
 }
